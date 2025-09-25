ffecb982bdd994d19f386b015e99f64b3c8897ca
Rohan Kumar <rohanvkumarv@gmail.com>
2025-09-25 17:56:34 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,13 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
+// 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverActions: {
-    bodySizeLimit: '10mb'  // Increase limit
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb'
-    }
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
